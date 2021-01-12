8859608d50f71942b04dec9fd0b33e39dae9082c
R-Stephenson <70218087+R-Stephenson@users.noreply.github.com>
2021-01-12 12:01:21 +0000

    Integrated a dynamic weather icon

--- src/Weather.js
+++ src/Weather.js
@@ -15,7 +15,7 @@ export default function Weather(props) {
             city: response.data.name,
             date: new Date(response.data.dt * 1000),
             description: response.data.weather[0].description,
-            iconURL: "https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png",
+            iconURL: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
             temperature: response.data.main.temp,
             feelsLike: response.data.main.feels_like,
             humidity: response.data.main.humidity,
