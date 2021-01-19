390a49d0921d5981eaeb724e653c61d45224eeef
R-Stephenson <70218087+R-Stephenson@users.noreply.github.com>
2021-01-19 16:20:16 +0000

    Enhanced Forecast
    Formatted date, added max and min temperatures

--- src/WeatherForecastPreview.js
+++ src/WeatherForecastPreview.js
@@ -6,12 +6,18 @@ export default function WeatherForecastPreview(props) {
 	function hours(){
 		let date = new Date(props.data.dt * 1000)
 		let hours = date.getHours();
+  	if (hours < 10) {
+    hours = `0${hours}`;
+  	}
+
 		return `${hours}:00`;
 	}	
 	
 	function temperature() {
-		let temperature = Math.round(props.data.main.temp);
-		return `${temperature}°C`;
+		let temperatureMax = Math.round(props.data.main.temp_max);
+		let temperatureMin = Math.round(props.data.main.temp_min);
+		return( <div><div><strong>↑ {temperatureMax}°C</strong></div>
+				<div><small>↓ {temperatureMin}°C</small></div></div>);
 	}
 
 	return (
