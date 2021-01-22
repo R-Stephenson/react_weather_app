ef208489fbb0cb57ca6a2f53bbaee517d0571cb1
R-Stephenson <70218087+R-Stephenson@users.noreply.github.com>
2021-01-22 00:30:40 +0000

    Updated CSS

--- src/WeatherForecastPreview.js
+++ src/WeatherForecastPreview.js
@@ -1,8 +1,6 @@
 import React from "react";
 import WeatherIcon from "./WeatherIcon";
 
-
-
 export default function WeatherForecastPreview(props) {
 	function hours(){
 		let date = new Date(props.data.dt * 1000)
